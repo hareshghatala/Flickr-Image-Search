0301df689ca2c9bfb6a2f7b1e2ecd36f6c402dfb
Hareshbhai Ghatala <hareshbhai.ghatala@absa.africa>
2021-08-31 13:22:06 +0530

    Added helper class

---
+++ Flickr Image Search/Helper/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  Flickr Image Search
+//
+//  Created by Haresh Ghatala on 2021/08/30.
+//
+
+public let flickrBaseURLString = "https://api.flickr.com/services"
+public let flickrApiKey = "11c40ef31e4961acf4f98c8ff4e945d7"
+public let apiDataLimit = 30
+
+public let columnSpace = 0
+public let defaultCellWidth = 120
+
+public let flickrFeedCellId = "FlickrImageCollectionViewCell"
+public let searchHistoryCellId = "SearchHistoryCell"
+
+public let infoMsgNoImgFound = "No results found."
+
+public let errorMsgInvalidEndpoint = "Invalid URL or Endpoint. Please retry."
+public let errorMsgInvalidResponse = "Invalid response or Data issue. Please retry."
+public let errorMsgUnknown = "Unknown Error. Please retry."
